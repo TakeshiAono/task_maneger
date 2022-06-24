2292c6e1be7f14b927887421b1b83bef5c785edd
aono <takeshi.aono001@gmail.com>
2022-06-24 16:12:02 +0900

    status model 作成　およびmigrate 完了

---
+++ db/migrate/20220624071108_create_statuses.rb
@@ -0,0 +1,10 @@
+class CreateStatuses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :statuses do |t|
+      t.references :task, null: false, foreign_key: true
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_24_070755) do
+ActiveRecord::Schema.define(version: 2022_06_24_071108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "statuses", force: :cascade do |t|
+    t.bigint "task_id", null: false
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_statuses_on_task_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -35,5 +43,6 @@ ActiveRecord::Schema.define(version: 2022_06_24_070755) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "statuses", "tasks"
   add_foreign_key "tasks", "users"
 end
