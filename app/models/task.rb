c88c12b6d19a3f0e7cc41014984178897419eba7
aono <takeshi.aono001@gmail.com>
2022-06-26 23:10:47 +0900

    タスクの詳細が空の場合　テスト実装完了

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   validates :title, presence: true
+  validates :priority, presence: true
+  validates :deadline, presence: true
   belongs_to :user
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,13 +3,16 @@ describe 'タスクモデル機能', type: :model do
   describe 'バリデーションのテスト' do
     context 'タスクのタイトルが空の場合' do
       it 'バリデーションにひっかる' do
-        task = User.create(name: "test", tasks_attributes: [{title: ""}])
+        task = User.create(name: "test", tasks_attributes: [{title: nil, priority: 1, deadline:Date.today}])
         expect(task).not_to be_valid
       end
     end
     context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
-        # ここに内容を記載する
+        priority = User.create(name: "test", tasks_attributes: [{title: "test", priority: nil , deadline:Date.today}])
+        deadline = User.create(name: "test", tasks_attributes: [{title: "test", priority: 1, deadline: nil}])
+        expect(priority).not_to be_valid
+        expect(deadline).not_to be_valid
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
