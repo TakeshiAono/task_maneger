2545e38add1cb8e3beefe3a3b7b10147b629ecd5
aono <takeshi.aono001@gmail.com>
2022-06-26 13:35:27 +0900

    model spec 生成完了

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,10 +1,5 @@
 FactoryBot.define do
-  factory :user do
-    # 下記の内容は実際に作成するカラム名に合わせて変更してください
-    name { 'user_name' }
-  end
-
-  factory :second_user, class: User do
-    name { 'user_name2' }
+  factory :task do
+    
   end
 end
