73293b01419def45757cc9ff4a727067adbedaa4
aono <takeshi.aono001@gmail.com>
2022-06-26 15:12:20 +0900

    カラムをtitle変更に伴うview controllerの変更実施

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -65,6 +65,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:user_id, :priority, :name, :deadline)
+      params.require(:task).permit(:user_id, :priority, :title, :deadline)
     end
 end
