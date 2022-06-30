64498a298c0b7d28cbd5384ed42b288f83dbb792
aono <takeshi.aono001@gmail.com>
2022-06-30 14:56:08 +0900

    titleとstatusのand検索テストの実装

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,11 +3,11 @@ describe 'タスクモデル機能', type: :model do
   describe '検索機能' do
     let!(:task){FactoryBot.create(:task, title: "test", status: "done")}
     let!(:second_task){FactoryBot.create(:second_task, title: 'sample', status: "not_yet")}
-    context 'scopメソッドでタイトルのあいまい検索をした場合' do
-      it '検索キーワードを含むタスクが絞り込まれる' do
-        expect(Task.search("title", "test")).to include(task)
-        expect(Task.search("title", "test")).not_to include(second_task)
-        expect(Task.search("title", "test").count).to eq 1
+    context 'scopメソッドでstatustとtitleのand検索をした場合' do
+      it '検索キーワードを含むsutatusとtitleが絞り込まれる' do
+        expect(Task.search("status", "done").search("title", "test")).to include(task)
+        expect(Task.search("status", "done").search("title", "test")).not_to include(second_task)
+        expect(Task.search("status", "done").search("title", "test").count).to eq 1
       end
     end
     context 'scopメソッドでstatusのあいまい検索をした場合' do
@@ -15,6 +15,13 @@ describe 'タスクモデル機能', type: :model do
         expect(Task.search("status", "done")).to include(task)
         expect(Task.search("status", "done")).not_to include(second_task)
         expect(Task.search("status", "done").count).to eq 1
+      end
+    end
+    context 'scopメソッドでタイトルのあいまい検索をした場合' do
+      it '検索キーワードを含むタスクが絞り込まれる' do
+        expect(Task.search("title", "test")).to include(task)
+        expect(Task.search("title", "test")).not_to include(second_task)
+        expect(Task.search("title", "test").count).to eq 1
       end
     end
   end
